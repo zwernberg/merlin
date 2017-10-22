24a7ebd1accf2b8538e8c24f8acff7a4c28068a8
Wernberg, Zachary <zawerny@gmail.com>
2017-10-22 17:46:03 -0500

    Added tribe to guests

--- merlin/guests/admin.py
+++ merlin/guests/admin.py
@@ -3,5 +3,7 @@ from merlin.guests.models import Guest
 
 # Register your models here.
 class guestAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'attending',)
+    list_display = ('__str__', 'tribe', 'attending',)
+    list_filter = ('tribe', 'attending')
+
 admin.site.register(Guest, guestAdmin)
--- merlin/guests/models.py
+++ merlin/guests/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
+TRIBE_CHOICES = (
+    ('0', 'Wernberg'),
+    ('1', 'Larson'),
+    ('2', 'Both'),
+)
 
 class Guest(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     attending = models.BooleanField(default=True)
+    tribe = models.CharField(max_length=1, choices=TRIBE_CHOICES, default = 2)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
