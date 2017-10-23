406c9c415d1418d895f87fdac3c1a4a2b6c1d532
Wernberg, Zachary <zawerny@gmail.com>
2017-10-22 20:45:18 -0500

    Added households

--- merlin/guests/admin.py
+++ merlin/guests/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from merlin.guests.models import Guest
+from merlin.guests.models import Guest, HouseHold
 
 # Register your models here.
 class guestAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'tribe', 'attending',)
+    list_display = ('__str__', 'tribe', 'attending', 'household')
     list_filter = ('tribe', 'attending')
 
+class GuestInline(admin.TabularInline):
+    model = Guest
+
+class HouseHoldAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'family_name', 'address',)
+    list_filter = ('family_name',)
+    inlines = [
+        GuestInline,
+    ]
+
 admin.site.register(Guest, guestAdmin)
+admin.site.register(HouseHold, HouseHoldAdmin)
--- merlin/guests/models.py
+++ merlin/guests/models.py
@@ -7,11 +7,19 @@ TRIBE_CHOICES = (
     ('2', 'Both'),
 )
 
+class HouseHold(models.Model):
+    family_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.family_name
+
 class Guest(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     attending = models.BooleanField(default=True)
     tribe = models.CharField(max_length=1, choices=TRIBE_CHOICES, default = 2)
+    household = models.ForeignKey(HouseHold, blank=True, null=True, related_name = 'guests')
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
