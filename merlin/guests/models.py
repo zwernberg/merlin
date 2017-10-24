56db981b66b6a7da06c0c627a96d98e59a2d10d7
Zach Wernberg <zawerny@gmail.com>
2017-10-23 19:53:58 -0500

    Add maybe option for attending

--- merlin/guests/models.py
+++ merlin/guests/models.py
@@ -15,9 +15,11 @@ class HouseHold(models.Model):
         return self.family_name
 
 class Guest(models.Model):
+    BOOL_CHOICES = (('0', 'No'), ('1', 'Yes'), ('3', 'Maybe'))
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    attending = models.BooleanField(default=True)
+    attending = models.CharField(default=1, choices=BOOL_CHOICES, max_length=1)
     tribe = models.CharField(max_length=1, choices=TRIBE_CHOICES, default = 2)
     household = models.ForeignKey(HouseHold, blank=True, null=True, related_name = 'guests')
 
