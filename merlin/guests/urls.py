6afb8345c887e5e3c616c8367fd171c3c6043fee
Zach Wernberg <zawerny@gmail.com>
2017-12-26 08:36:04 -0600

    removed old view

--- merlin/guests/urls.py
+++ merlin/guests/urls.py
@@ -3,9 +3,4 @@ from django.conf.urls import url
 from merlin.guests import views
 
 urlpatterns = [
-    url(
-        regex=r'^$',
-        view=views.GuestListView.as_view(),
-        name='list'
-    ),
 ]
